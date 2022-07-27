4ff831d204777417e3e0111378a254065fbde582
diegoabud92 <diegoabud92@hotmail.com>
2022-07-27 16:52:50 -0300

    alpha

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Para que ante un validation error, no me agregue la clase "field_with_errors" en el input con error
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+    html_tag.html_safe
+end
